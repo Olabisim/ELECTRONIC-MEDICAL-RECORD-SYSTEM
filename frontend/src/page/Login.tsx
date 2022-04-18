b89663e0f29100a83c9905f8882e3222062a10e4
holabisii <ajoseholabisi@gmail.com>
2022-04-18 04:03:28 -0400

    login animation with useref

--- frontend/src/page/Login.tsx
+++ frontend/src/page/Login.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import '../css/login.css'
 
 export const Login = () => {
+
+        const [toggle, setToggle] = useState<String>('rect2')
+
+        const username = useRef<HTMLInputElement>(null)
+        const password = useRef<HTMLInputElement>(null)
+        const rect = useRef<SVGSVGElement>(null)
+
+        
+        const handle1 = () => {
+                
+                console.log(rect.current)
+                // console.log(rect.current.setAttribute)
+                const { current } = rect;
+                setToggle('rect1')
+                // current.setAttribute("class", "rect2");
+        }
+
+        const handle2 = () => {
+                console.log("this is handle2")
+                // rect.setAttribute("class", "rect1");
+                setToggle('rect2')
+        }
+
         return(
                 <>
 
@@ -11,18 +34,20 @@ export const Login = () => {
                         
                         <div className="header">Sign In</div>
                         <div className='info'>*Click on the input boxes</div>
-                        <input id='username' className='text inc2' 
-                                // onFocus="handle2()" 
+                        <input ref={username} className={`text inc2 ${!toggle && 'rect2'}`}
+                                onFocus={handle2}
+                                onClick={handle2}
                                 type="text" name="Username" placeholder='Username' value="" /> 
                         {/* <!-- Had to remove the type "password" due to the browser user credential's autofill--> */}
 
-                        <input id='password' className='pass inc1' 
-                        // onfocus="handle1()"
+                        <input ref={password} className={`pass inc1 ${toggle && 'rect1'}`} 
+                        onFocus={handle1}
+                        onClick={handle1}
                         type="pass" name="Password" placeholder='Password' value="" />
 
                         <button>Sign In</button>
 
-                        <svg width="390" height="549" xmlns="http://www.w3.org/2000/svg" >
+                        <svg width="390" height="549" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" ref={rect} className={`${toggle}`} >
                                 <rect id='rect' className='rect1'   x="45px"  y="300px"   rx="27" ry="27" width="300px" height="50px" style={{stroke: '#fff', strokeWidth: '1px', fill: '#000'}} />
                         </svg>
                 </div>
