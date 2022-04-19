f171f136f127f6719e39042f2785246b1ec0889b
Daniel <taiwodaniel2020@gmail.com>
2022-04-19 07:59:00 +0100

    connecting api to database

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./config.env",
+});
+
 const app = express();
 
 app.get("/", (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,16 @@
+import mongoose from "mongoose";
 import app from "./app.js";
+
+const DATABASE = process.env.DATABASE;
+
+mongoose
+  .connect(DATABASE)
+  .then((connection) => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const port = process.env.PORT || 7000;
 app.listen(port, () => {
