f36e6c18dbe2731e63e4795fbaf842d7e22e552b
Daniel <taiwodaniel2020@gmail.com>
2022-04-21 09:09:37 +0100

    finishing the basic crud opereations

--- backend/controllers/students.js
+++ backend/controllers/students.js
@@ -5,12 +5,31 @@ const getStudents = async (req, res) => {
     const students = await Student.find();
     res.status(200).json({
       status: "success",
+      result: students.length,
       data: {
         students,
       },
     });
   } catch (err) {
     res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+const getStudent = async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        student,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
       status: "fail",
       message: err,
     });
@@ -35,4 +54,51 @@ const createStudent = async (req, res) => {
   }
 };
 
-export { getStudents, createStudent };
+const updatedStudent = async (req, res) => {
+  try {
+    const updatedStudent = await Student.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        student: updatedStudent,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+const deleteStudent = async (req, res) => {
+  try {
+    await Student.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+export {
+  getStudents,
+  createStudent,
+  getStudent,
+  updatedStudent,
+  deleteStudent,
+};
--- backend/routes/students.js
+++ backend/routes/students.js
@@ -1,8 +1,19 @@
 import express from "express";
 const router = express.Router();
 
-import { getStudents, createStudent } from "../controllers/students.js";
+import {
+  getStudents,
+  createStudent,
+  getStudent,
+  updatedStudent,
+  deleteStudent,
+} from "../controllers/students.js";
 
 router.route("/").get(getStudents).post(createStudent);
+router
+  .route("/:id")
+  .get(getStudent)
+  .patch(updatedStudent)
+  .delete(deleteStudent);
 
 export default router;
