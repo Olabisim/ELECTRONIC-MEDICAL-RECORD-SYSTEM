0b857c4004d5b39a44a71c3070220d2e2a72ccbd
holabisii <ajoseholabisi@gmail.com>
2022-05-28 19:35:00 -0400

    ui for stats page done

--- frontend/src/page/Stats.tsx
+++ frontend/src/page/Stats.tsx
@@ -1,15 +1,25 @@
 import React from "react";
-
+import '../css/stats.css'
 
 export const Stats = () => {
         return (
-                <div>
+                <div className="stats parent">
                         <div>
                                 <img src="../../STATS.svg" alt="" />
                         </div>
-                        <div>
-                                
-                                <h2>TOTAL STUDENTS: </h2>
+                        <div> 
+                                <div className="stats card">
+
+                                        <h2>TOTAL STUDENTS </h2>
+                                        <h1>123</h1>
+                                        <button className="stats button">view</button>
+                                </div>
+                                <div className="stats card">
+
+                                        <h2>TOTAL PATIENTS </h2>
+                                        <h1>1234</h1>
+                                        <button className="stats button">view</button>
+                                </div>
                         </div>
                 </div>
         )
