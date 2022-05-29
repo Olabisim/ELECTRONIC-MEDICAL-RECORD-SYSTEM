819c8ebf6498ce818e041eb2d6a78e0c5f123e4a
Daniel <taiwodaniel2020@gmail.com>
2022-05-29 08:36:27 +0100

    adding more fields to the user model

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -4,33 +4,37 @@ const { Schema, model } = mongoose;
 const studentSchema = new Schema({
   surname: {
     type: String,
-    required: [true, "Every student must have a name"],
+    required: [true, "Provide all required information"],
     trim: true,
   },
   otherNames: {
     type: String,
-    required: [true, "Every student must have name"],
+    required: [true, "Provide all required information"],
     trim: true,
+  },
+  matricNumber: {
+    type: String,
+    required: [true, "Provide all required information"],
   },
   yearOfAdmission: {
     type: String,
-    required: [true, "Year of admission is a required field"],
+    required: [true, "Provide all required information"],
   },
   faculty: {
     type: String,
-    required: [true, "Faculty field is required"],
+    required: [true, "Provide all required information"],
   },
   department: {
     type: String,
-    required: [true, "Faculty field is required"],
+    required: [true, "Provide all required information"],
   },
   religion: {
     type: String,
-    required: true,
+    required: [true, "Provide all required information"],
   },
   telPhone: {
     type: "String",
-    required: true,
+    required: [true, "Provide all required information"],
   },
   date: {
     type: Date,
@@ -38,52 +42,81 @@ const studentSchema = new Schema({
   },
   homeAddress: {
     type: String,
-    required: true,
+    required: [true, "Provide all required information"],
     trim: true,
   },
   gender: {
     type: String,
-    required: [true, "A student has to have a gender"],
+    required: [true, "Provide all required information"],
   },
   dateOfBirth: {
     type: Date,
-    required: [true, "DOB is a required field"],
+    required: [true, "Provide all required information"],
   },
   nationality: {
     type: String,
-    required: [true, "Nationality is a required field"],
+    required: [true, "Provide all required information"],
   },
   ethnicGroup: {
     type: String,
-    required: [true, "Ethnicity is a required field"],
+    required: [true, "Provide all required information"],
   },
   maritalStatus: {
     type: String,
-    required: [true, "Marital status is a required field"],
+    required: [true, "Provide all required information"],
   },
   guardianName: {
     type: String,
-    required: [true, "Guardian name is a required field"],
+    required: [true, "Provide all required information"],
     trim: true,
   },
   guardianOffAdd: {
     type: String,
-    required: [true, "Guardian office address is a required field"],
+    required: [true, "Provide all required information"],
     trim: true,
   },
   guardianTelNum: {
     type: String,
-    required: [true, "Guardian telephone number is a required field"],
+    required: [true, "Provide all required information"],
   },
   guardianResAdd: {
     type: String,
-    required: [true, "Guardian residence address is a required field"],
+    required: [true, "Provide all required information"],
     trim: true,
   },
   guardianRel: {
     type: String,
-    required: [true, "Guardian relationship is a required field"],
+    required: [true, "Provide all required information"],
     trim: true,
+  },
+  userType: {
+    type: String,
+    enum: {
+      values: ["student", "adminUser", "admin"],
+      message: "User has to either be a student, adminUser or an admin",
+    },
+    default: "student",
+  },
+  password: {
+    type: String,
+    required: [true, "Enter Password"],
+    minlength: [8, "Password must be atleast 8 characters long"],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Enter password again to confirm"],
+    validate: {
+      validator: function (cPassword) {
+        return cPassword == this.password;
+      },
+      message: "Passwords doesn't match",
+    },
+  },
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
   },
 });
 
