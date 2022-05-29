565567f9487fe0fe32a90e93eb923e2113062ef1
holabisii <ajoseholabisi@gmail.com>
2022-05-29 08:34:06 -0400

    linked student registration

--- frontend/src/page/StudentRegister.tsx
+++ frontend/src/page/StudentRegister.tsx
@@ -8,15 +8,14 @@ export const StudentRegister = () => {
                 <div className="StudentRegister_main_flex">
                         <div className="StudentRegister_main">
                                 <p >
-                                
-                                You must complete this as records are not stored half way done.<br /><br />
-                                NOTE:: students must report to the Health Center to take measurements for completion of the registration.
+                                        You must complete this as records are not stored half way done.<br /><br />
+                                        NOTE:: students must report to the Health Center to take measurements for completion of the registration.
                                 </p>
-                                <button className="stats button">START REGISTRATION</button>
+                                <button className="stats button"><Link to="/student/first">START REGISTRATION</Link></button>
                                 <div>
                                         <button  className="StudentRegister_link">
 
-                                                        <Link to="/student/login">go back</Link>
+                                                <Link to="/student/login">go back</Link>
                                                 
                                         </button>
                                 
