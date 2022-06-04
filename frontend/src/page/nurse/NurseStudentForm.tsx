7dfc0ca10c451f8dd3874a50b819b014c2496388
holabisii <ajoseholabisi@gmail.com>
2022-06-04 19:48:53 -0400

    useState for nursestudentform

--- frontend/src/page/nurse/NurseStudentForm.tsx
+++ frontend/src/page/nurse/NurseStudentForm.tsx
@@ -1,14 +1,22 @@
 
+import { useState } from "react"
+
 
 export const NurseStudentForm = () => {
+
+        const [bplevel, setBplevel] = useState<string>('')
+        const [healthStatus, setHealthStatus] = useState<string>('')
+
+        console.log(setBplevel)
+
         return (
                 <div>
                         <div className="NSFlogin">
                                 <h1 style={{fontWeight: 100}}>AJOSEH OLABISI MINASU</h1>
                                 <h1>17059192</h1>
                                 <form method="post" action="">
-                                <p><input type="text" name="login" value="" placeholder="bp level" className="NSF" /></p>
-                                <p><input type="text" name="password" value="" placeholder="health status" className="NSF" /></p>
+                                <p><input type="text" value={bplevel} placeholder="bp level" className="NSF" onChange={(e) => setBplevel(e.target.value) } /></p>
+                                <p><input type="text" name="password" value={healthStatus} placeholder="health status" className="NSF" onChange={(e) => setHealthStatus(e.target.value) } /></p>
 
                                 {/* <p className="remember_me">
                                 <label>
