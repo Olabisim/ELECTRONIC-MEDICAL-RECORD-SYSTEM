210eece4ae60efcf53cc2b8a80db974bf4d4497d
holabisii <ajoseholabisi@gmail.com>
2022-06-13 12:08:38 -0400

    destructured the path file

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
-import path from "path";
+import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 import express from "express";
 import cors from "cors";
